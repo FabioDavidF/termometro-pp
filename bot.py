4795ae1ff80e10b26d27fe94b5c073999baed158
Fábio David Freitas <fabio.david.contato@gmail.com>
2021-03-19 21:11:51 -0300

    Versão alpha

--- bot.py
+++ bot.py
@@ -2,16 +2,19 @@ from time import sleep
 from selenium.webdriver import Chrome
 from consulta import pegarDados, pegarHora
 
-def criarTweet(temperature, description, date, time, condition):
+
+def criarTweet(temperature, description, date, condition):
+    # Pegando a hora
+    time = pegarHora()
     # Variantes de temperatura
     if int(temperature) > 32:
-        temperature_variant = 'Calor pra caralho'
+        temperature_variant = 'Tá calor pra caralho'
     if 25 < int(temperature) < 32:
-        temperature_variant = 'Calor suportável'
+        temperature_variant = 'Tá um calor suportável'
     if 20 < int(temperature) < 25:
-        temperature_variant = 'Fresquinho'
+        temperature_variant = 'Tá fresquinho'
     if int(temperature) < 20:
-        temperature_variant = 'TA FAZENDO FRIO EM PRUDENTE'
+        temperature_variant = 'TÁ FAZENDO FRIO EM PRUDENTE'
 
     # Variantes de condição temporal
     condition_variant = ''
@@ -22,11 +25,20 @@ def criarTweet(temperature, description, date, time, condition):
     
     # Montando o tweet com as variantes
     if condition_variant != '':
-        tweet_string = f'{temperature}°C, {temperature_variant}, {description}\n\n Tá {condition_variant}\n\n{time}'
+        tweet_string = f'{temperature}°C\n{temperature_variant}\n{description}\n\n Tá {condition_variant}\n\n{time}'
     else:
-        tweet_string = f'{temperature}°C, {temperature_variant}, {description}\n\n{time}'
+        tweet_string = f'{temperature}°C\n{temperature_variant}\n{description}\n\n{time}'
 
     return tweet_string
+
+def criarTweetPrevisao(min_temp, max_temp, condition, dia):
+    # Pegando a hora
+    time = pegarHora()
+
+    if ('storm' in condition) or ('rain' in condition):
+        return f'Previsão do tempo de amanhã ({dia}): \n\nVai chover!\nTemperatura mínima: {min_temp}°C\nTemperatura máxima: {max_temp}°C\n\n{time}'
+    else:
+        return f'Previsão do tempo de amanhã ({dia})\nTemperatura mínima: {min_temp}°C\nTemperatura máxima: {max_temp}°C\n\n{time}'
 
     
 class TermometroBot():
@@ -47,15 +59,21 @@ class TermometroBot():
         login_button.click()
         sleep(4)
     
-    def tweet(self):
+    def tweet(self, tipo):
         data = pegarDados(self.key)
-        temperature = data['results']['temp']
-        description = data['results']['description']
-        date = data['results']['date']
-        time = pegarHora()
-        condition = data['results']['condition_slug']
-        
-        tweet_string = criarTweet(temperature, description, date, time, condition)
+
+        if tipo == 'atual':
+            temperature = data['results']['temp']
+            description = data['results']['description']
+            date = data['results']['date']
+            condition = data['results']['condition_slug']
+            tweet_string = criarTweet(temperature, description, date, condition)
+        elif tipo == 'previsao':
+            min_temp = data['results']['forecast'][1]['min']
+            max_temp = data['results']['forecast'][1]['max']
+            condition = data['results']['forecast'][1]['condition']
+            dia = data['results']['forecast'][1]['weekday']
+            tweet_string = criarTweetPrevisao(min_temp, max_temp, condition, dia)
         tweet_input = self.nav.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
         tweet_input.send_keys(tweet_string)
         tweet_button = self.nav.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]/div/span/span')
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import os
 from bot import TermometroBot
+from time import sleep
+from datetime import datetime
+
+
+def pegarSoHora():
+    objeto = datetime.now()
+    hora = objeto.hour
+    return hora
 
 
 username = os.getenv('TWITTER_USER')
 password = os.getenv('TWITTER_PASSWORD')
 key = os.getenv('HG_KEY')
+ja_fez_previsao = bool
 
 bot = TermometroBot(username, password, key)
 bot.login()
-bot.tweet()
+while True:
+    hora_atual = pegarSoHora()
+    if hora_atual == 22:
+        bot.tweet('previsao')
+    elif hora_atual in [9, 12, 15, 18, 21]:
+        bot.tweet('atual')
+    sleep(3600)
